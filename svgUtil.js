9dcf10e5aa657ac1b02e6c567ac6a50ab9edba85
System Administrator <root@dvm-macbook.local>
2014-04-15 20:28:30 +0400

    svgUtil Initial

---
+++ svgUtil.js
@@ -0,0 +1,14 @@
+/*
+    SVG util
+*/
+
+var _       = require('lodash');
+var XMLDOMParser = require('xmldom').DOMParser;
+var SvgPath = require('svgpath');
+
+var svgUtil = function(svgData) {
+    
+};
+
+module.exports.svgUtil = svgUtil;
+
