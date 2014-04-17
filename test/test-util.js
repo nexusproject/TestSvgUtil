97d820e74a79adef0ec1c3ba1213aeef0761d915
Dmitry Sergeev <realnexusway@gmail.com>
2014-04-17 14:36:48 +0400

    extending tests

--- test/test-util.js
+++ test/test-util.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
 var fs = require('fs');
-var svgPrune = require('../svgPrune').svgPrune;
+var svgPrune = require('svgprune').svgPrune;
 
 var svgFile = process.argv[2];
+var svgOutFile = process.argv[3];
+
 if (! svgFile) {
-    console.log("Usage: test-util.js <somefile.svg>");
+    console.log("Usage: test-util.js <in.svg> <out.svg>");
     process.exit();
 }
 
@@ -15,5 +17,18 @@ if (! fs.existsSync(svgFile)) {
 }
 
 var t = fs.readFileSync(svgFile).toString();
-console.log( svgPrune( t ) );
+var res = svgPrune(t);
+
+console.log(res);
+
+if (svgOutFile) {
+    var svgdata = '<?xml version="1.0" standalone="no"?>\n<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" ';
+    svgdata += '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n';
+    svgdata += '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" ';
+    svgdata += 'height="'+res.height+'" ' + 'width="' + res.width + '">\n';
+    svgdata += '<path d="'+res.path+'" />\n'
+    svgdata += '</svg>'
+
+    fs.writeFileSync(svgOutFile, svgdata)
+}
 
